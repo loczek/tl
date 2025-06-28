5181e9278b166678c2cc20c81072a50dd9702410
loczek <30776250+loczek@users.noreply.github.com>
2025-06-28 21:22:19 +0200

    add prod or dev text

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,9 +1,10 @@
 function Footer() {
   return (
-    <footer className="w-full py-4 px-8 rounded-2xl bg-[#141414]">
+    <footer className="w-full py-4 px-8 rounded-2xl bg-[#141414] flex justify-between items-center">
       <span className="py-2 block text-center">
         Made by <span className="opacity-60">loczek</span>
       </span>
+      {import.meta.env.PROD ? "prod" : "dev"}
     </footer>
   );
 }
