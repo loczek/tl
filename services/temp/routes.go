b316cfc0d7731a8627c7d253d6c61ff7591d9dfb
loczek <30776250+loczek@users.noreply.github.com>
2025-07-25 18:13:33 +0200

    add temp traces

--- services/temp/routes.go
+++ services/temp/routes.go
@@ -1,10 +1,14 @@
 package temp
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/gofiber/fiber/v2"
+	"go.opentelemetry.io/otel"
 )
+
+var tracer = otel.Tracer("github.com/loczek/go-link-shortener")
 
 type Handler struct {
 	logger *slog.Logger
@@ -15,6 +19,15 @@ func NewHandler(logger *slog.Logger) *Handler {
 }
 
 func (h *Handler) Temp(c *fiber.Ctx) error {
+	ctx, span := tracer.Start(context.Background(), "before")
+	defer span.End()
+
+	h.logger.Info("log from temp handler")
+	h.logger.InfoContext(ctx, "log from temp handler with ctx")
+
+	expensiveCalculation(ctx, 1, 100_000)
+	expensiveCalculationTwo(ctx, 1, 100_000)
+
 	return c.SendStatus(200)
 }
 
@@ -33,4 +46,30 @@ func (h *Handler) Panic(c *fiber.Ctx) error {
 
 func (h *Handler) Err(c *fiber.Ctx) error {
 	return fiber.ErrBadGateway
+}
+
+func expensiveCalculation(ctx context.Context, one int, two int) int {
+	ctx, span := tracer.Start(ctx, "calc")
+	defer span.End()
+
+	ans := 0
+
+	for i := one; i < two; i++ {
+		ans += i
+	}
+
+	return ans
+}
+
+func expensiveCalculationTwo(ctx context.Context, one int, two int) int {
+	ctx, span := tracer.Start(ctx, "lookup user")
+	defer span.End()
+
+	ans := 0
+
+	for i := one; i < two; i++ {
+		ans += i
+	}
+
+	return ans
 }
