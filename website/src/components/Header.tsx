c9514f64aaa708980969a64e221adc7e532d7efa
loczek <30776250+loczek@users.noreply.github.com>
2025-08-26 00:21:34 +0200

    update github link

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -1,12 +1,12 @@
-import logo from "../assets/logo.svg";
 import github from "../assets/icons/github.svg";
+import logo from "../assets/logo.svg";
 
 function Header() {
   return (
     <header className="w-full py-4 px-8 rounded-2xl bg-dark-900 flex justify-between">
       <img src={logo} alt="logo" className="size-10" />
       <a
-        href="https://github.com/42fm/42fm"
+        href="https://github.com/loczek/tl"
         className="p-2 bg-dark-700 rounded-lg hover:bg-dark-600 transition-colors"
       >
         <img src={github} alt="github repo" className="size-6" />
