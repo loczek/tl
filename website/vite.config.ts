c436f027f1cbcc99e1ed8b8033506f046016c669
loczek <30776250+loczek@users.noreply.github.com>
2025-10-13 17:54:08 +0200

    add host for development

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    allowedHosts: ["short.com"],
     host: "0.0.0.0",
-    // proxy: {
-    //   "/api": {
-    //     target: "localhost:3000",
-    //   },
-    // },
   },
 });
