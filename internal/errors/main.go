50673d69d7a6677b339d318696b5abd26288aef0
loczek <30776250+loczek@users.noreply.github.com>
2025-10-19 08:34:05 +0200

    add unsupported schema error

--- internal/errors/main.go
+++ internal/errors/main.go
@@ -1,11 +1,12 @@
 package api_errors
 
 const (
-	InvalidBodyShape = "invalid body"
-	InvalidBodyData  = "invalid request data"
-	InvalidUrl       = "invalid url"
-	InternalServer   = "internal server error"
-	NotFound         = "not found"
-	UrlNotFound      = "url not found"
-	DatabaseQuery    = "could not retrieve data from database"
+	InvalidBodyShape  = "invalid body"
+	InvalidBodyData   = "invalid request data"
+	InvalidUrl        = "invalid url"
+	InternalServer    = "internal server error"
+	NotFound          = "not found"
+	UrlNotFound       = "url not found"
+	DatabaseQuery     = "could not retrieve data from database"
+	UnsupportedSchema = "unsupported schema"
 )
